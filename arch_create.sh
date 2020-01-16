a4888bc5b0cea4e5ff4d790344b72e75aa7f67b4
godlikegit <38789340@qq.com>
2020-01-16 14:38:52 +0800

    Update and rename arch_install.sh to arch_create.sh

---
+++ arch_create.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#dd if=/dev/zero of=/dev/sda bs=512K count=1
+
+#Partition the disks
+echo -e "n\n\n\n\n+1G\nn\n\n\n\n+8G\nn\n\n\n\n\nw\n" | fdisk /dev/sda
+
+#Format the partitions
+mkswap /dev/sda1
+mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda3
+
+#Mount the file systems
+swapon /dev/sda1
+mount /dev/sda2 /mnt
+mkdir /mnt/home
+mount /dev/sda3 /mnt/home
+
+#Select the mirrors
+cd /etc/pacman.d
+cp mirrorlist _mirrorlist
+cat _mirrorlist | grep China -A 1 | grep -v '-' > mirrorlist
+rm _mirrorlist
+cp mirrorlist /mnt/ect/pacman.d
+cd
+
+#Install essential packages
+pacstrap /mnt base vim grub dhcpcd
